a96f39466a4c9ef34898f916f02ad245bf49af76
pranav-0090 <64990760+pranav-0090@users.noreply.github.com>
2022-02-15 14:02:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express');
 
 const app = express();
 
+const http = require('http');
+const server = http.createServer((req, res)=>{
+   console.log(req.url, req.method, req. headers);
+   res.write('<html>');
+   res.write('<head> <title> Hello TutorialsPoint </title> </head>');
+   res.write(' <body> Hello Tutorials Point </body>');
+   res.write('</html>');
+   //write end to mark it as stop for node js response.
+   res.end();
+});
+
 app.get('/', (req, res) => res.send('App Running Sending data...')); //res.send
 
 const PORT = process.env.PORT | 3000
